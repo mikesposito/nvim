86e7942ab5635d08de8dbf0be1e4bd53373f38ec
Michele Esposito <34438276+mikesposito@users.noreply.github.com>
2024-08-18 13:00:40 +0200

    refactor: better organised keymaps (#17)
    * wip: move bindings to different file
    
    * refactor: move keymaps to main

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 -- Author: @mikesposito
 
 -- Load nvim preferences
-require 'settings.preferences'
+require 'preferences.general'
+require 'preferences.keymaps'
 
 -- Load lazy.nvim plugin and all the plugins configurations
 require 'main.lazy'
-
--- Load keymaps
--- Needs to be loaded after the plugins configurations
-require 'settings.keymaps'
