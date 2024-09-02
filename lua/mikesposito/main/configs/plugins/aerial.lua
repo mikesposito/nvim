0d110900c3284e3da9772952f6738dd9f6bae213
Michele Esposito <michele@esposito.codes>
2024-09-02 12:39:16 +0200

    feat: enable `cpp` lang support

--- lua/mikesposito/main/configs/plugins/aerial.lua
+++ lua/mikesposito/main/configs/plugins/aerial.lua
@@ -4,6 +4,7 @@ return {
   'stevearc/aerial.nvim',
   opts = {
     open_automatic = P.WINDOW_OPEN_OUTLINE_ON_STARTUP,
+    filter_kind = false,
   },
   -- Optional dependencies
   dependencies = {
