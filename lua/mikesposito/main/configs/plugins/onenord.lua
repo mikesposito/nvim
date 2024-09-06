5ed6912520e4f241b194c68297d28af212a693a2
Michele Esposito <michele@esposito.codes>
2024-09-06 14:36:03 +0200

    fix: theme colorscheme var

--- lua/mikesposito/main/configs/plugins/onenord.lua
+++ lua/mikesposito/main/configs/plugins/onenord.lua
@@ -3,6 +3,6 @@ local P = require 'mikesposito.preferences.general'
 return {
   'rmehri01/onenord.nvim',
   config = function()
-    require('onenord').setup { theme = P.THEME_FLAVOUR }
+    require('onenord').setup { theme = P.THEME_COLOR_SCHEME }
   end,
 }
