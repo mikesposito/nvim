7796333d6f049af0efe83451ce1fc44b7e615293
Michele Esposito <michele@esposito.codes>
2025-11-11 00:09:07 +0100

    disable rust-tools

--- lua/mikesposito/main/configs/lang.lua
+++ lua/mikesposito/main/configs/lang.lua
@@ -3,10 +3,4 @@
 return {
   -- LSP
   require 'mikesposito.main.configs.plugins.lspconfig',
-
-  -- Rust
-  require 'mikesposito.main.configs.plugins.rust-tools',
-
-  -- Yarn tools
-  -- require 'mikesposito.main.configs.plugins.package-info',
 }
